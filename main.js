37d869962f05d7ce9c5ea6ea6f401f165f425586
Rayane de Oliveira <rayanecontato1@gmail.com>
2023-05-30 20:21:39 -0300

    adiciona elementos no vetor

--- main.js
+++ main.js
@@ -10,11 +10,20 @@ atualiza();  //coloca o valor no inicio quando abre a pagina
 function envia(evento) {
     evento.preventDefault();
     console.log('Formulário enviado!');
-    store.estado++;
+    const n = form.valor.value;
+    store.estado.push(n);
+    form.valor.value = "";
+    form.valor.focus();
     atualiza();
 }
 
 function atualiza(){
-    const ol = document.querySelector('ol'); 
-    ol.innerHTML = `<li>${store.estado}</li>`;  // mudei o html do elemento
+    const ol = document.querySelector('ol');
+    ol.innerHTML = "";
+    for(let i=0; i< store.estado.length; i++){
+    const li = document.createElement('li');
+    li.textContent = store.estado[i];
+    ol.appendChild(li);
+    }
 }
+
