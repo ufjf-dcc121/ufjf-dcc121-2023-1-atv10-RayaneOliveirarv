8b874f4d3f4f1850e047b2890b7d8dec4efd2ce1
Rayane de Oliveira <rayanecontato1@gmail.com>
2023-05-30 19:56:21 -0300

    adiciona exportação padrão

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {estado} from "./store.js";
+import store from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
@@ -10,11 +10,11 @@ atualiza();  //coloca o valor no inicio quando abre a pagina
 function envia(evento) {
     evento.preventDefault();
     console.log('Formulário enviado!');
-    estado++;
+    store.estado++;
     atualiza();
 }
 
 function atualiza(){
     const ol = document.querySelector('ol'); 
-    ol.innerHTML = `<li>${estado}</li>`;  // mudei o html do elemento
+    ol.innerHTML = `<li>${store.estado}</li>`;  // mudei o html do elemento
 }
